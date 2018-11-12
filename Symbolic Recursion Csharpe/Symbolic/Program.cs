faa3837c9d2763b5c411f3a65a80ff40da4cf378
Maikel SITBON <maikelsitbon@gmail.com>
2018-11-12 14:58:06 +0200

    Add files via upload

---
+++ Symbolic Recursion Csharpe/Symbolic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Symbolic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Symbolic("%#", 10);
+            Console.WriteLine();
+
+        }
+        static void Symbolic (string symbole, int iteration)
+        {
+            Console.Write(symbole[iteration%2]);
+            if(iteration>1)Symbolic(symbole, iteration - 1);
+        }
+    }
+}
